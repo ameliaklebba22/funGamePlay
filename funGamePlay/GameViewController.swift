a483d4d695225f76f78ec70a9ca030c825e5ab35
Amelia <amelia.klebba@gmail.com>
2022-05-11 07:13:19 -0500

    fun

--- funGamePlay/GameViewController.swift
+++ funGamePlay/GameViewController.swift
@@ -11,49 +11,48 @@ import GameplayKit
 
 class GameViewController: UIViewController {
 
+    var play: GameScene!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Load 'GameScene.sks' as a GKScene. This provides gameplay related content
-        // including entities and graphs.
+        
+        let value = UIInterfaceOrientation.landscapeLeft.rawValue
+                    UIDevice.current.setValue(value, forKey: "orientation")
+
         if let scene = GKScene(fileNamed: "GameScene") {
-            
-            // Get the SKScene from the loaded GKScene
             if let sceneNode = scene.rootNode as! GameScene? {
-                
-                // Copy gameplay related content over to the scene
-                sceneNode.entities = scene.entities
-                sceneNode.graphs = scene.graphs
-                
-                // Set the scale mode to scale to fit the window
+                play = sceneNode
                 sceneNode.scaleMode = .aspectFill
-                
-                // Present the scene
                 if let view = self.view as! SKView? {
                     view.presentScene(sceneNode)
-                    
                     view.ignoresSiblingOrder = true
-                    
                     view.showsFPS = true
                     view.showsNodeCount = true
                 }
-            }
-        }
+                }
+                }
     }
 
     override var shouldAutorotate: Bool {
-        return true
+    return true
     }
 
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
+    return .landscapeRight
     }
 
     override var prefersStatusBarHidden: Bool {
-        return true
+    return true
     }
+
+
+
+
+    @IBAction func buttonFun(_ sender: Any) {
+    play.jump()
+    }
+    
+
+
 }
